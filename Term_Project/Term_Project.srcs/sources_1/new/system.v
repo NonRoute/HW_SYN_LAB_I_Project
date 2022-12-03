b15a8e119620fc7bce7c9faf0b224627a5796ef1
Chanon Rattanajaratkul <chanon45559@outlook.com>
2022-12-03 21:15:23 +0700

    create project

---
+++ Term_Project/Term_Project.srcs/sources_1/new/system.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12/03/2022 09:13:35 PM
+// Design Name: 
+// Module Name: system
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module system(
+    input wire RsRx,
+    output wire [3:0]vgaRed, vgaGreen, vgaBlue,
+    output wire Hsync, Vsync,
+    input btnC, clk
+    );
+endmodule
